e1e921369c854b3cbb1cae01baf9663e8efac3b9
tdamsma <tdamsma@gmail.com>
2015-08-21 08:35:01 +0200

    test with 10k elems in subscription

--- client/components.jsx
+++ client/components.jsx
@@ -1,5 +1,8 @@
 PostSubs = new SubsManager();
 
+Meteor.subscribe('allPosts');
+
+
 MainLayout = React.createClass({
   render() {
     return <div>
@@ -10,16 +13,59 @@ MainLayout = React.createClass({
   }
 });
 
-BlogHome = React.createClass({
+
+
+PostRow = React.createClass({
   render() {
-    return <div>
-      <p>This is the home page of our blog</p>
-      <p>
-        <a href="/hello-world">See Hello World Post</a>
-      </p>
-    </div>;
+    return <li>
+      <a href={"/" + this.props.post._id}>{this.props.post.title}</a></li>
   }
 });
+
+BlogHome = React.createClass({
+  mixins: [ReactMeteorData],
+  getInitialState: function() {
+      return {
+          filterText: 'a'
+      };
+  },
+  handleUserInput: function(filterText) {
+      this.setState({
+          filterText: filterText
+      });
+  },
+  getMeteorData() {
+    searchString = new RegExp('^title[0-9]* ' + this.state.filterText); 
+    return {
+      count: Posts.find({}).count(),
+      posts: Posts.find({
+        title:{$regex:searchString}
+      },{
+        limit: 10,
+        sort: {createdAt:-1}
+      }).fetch()
+    };
+  },
+  renderPosts() {
+    return this.data.posts.map((post) => {
+      return <PostRow key={post._id} post={post} />;
+    });
+  },
+  render() {
+    return <div>
+        <p>Postes, subscribed: ({this.data.count})</p>
+        <SearchBar
+                    filterText={this.state.filterText}
+                    onUserInput={this.handleUserInput}
+                />
+        <ul>
+          {this.data.posts? this.renderPosts():<p>Loading...</p>}
+        </ul>
+      </div>;
+  }
+});
+
+
 
 BlogPost = React.createClass({
   mixins: [ReactMeteorData],
@@ -27,14 +73,19 @@ BlogPost = React.createClass({
     var data = {};
     var postId = this.props.postId;
     var handle = PostSubs.subscribe('singlePost', postId);
-    if(handle.ready()) {
-      data.post = Posts.findOne({_id: postId});
+    data.post = Posts.findOne({_id: postId});
+    if (!handle.ready()) {
+      if (data.post){
+        if (!data.post.content){
+          data.post.content = 'Loading...'}
+        }
     }
     return data;
   },
   getContent() {
     return <div>
       <h3>{this.data.post.title}</h3>
+      by <h4>{this.data.post.user}</h4>
       <p>{this.data.post.content}</p>
     </div>;
   },
@@ -43,5 +94,28 @@ BlogPost = React.createClass({
       <a href="/">Back</a>
       {this.data.post? this.getContent() : <p>Loading...</p>}
     </div>
+
   }
+});
+
+
+var SearchBar = React.createClass({
+    handleChange: function() {
+        this.props.onUserInput(
+            this.refs.filterTextInput.getDOMNode().value
+        );
+    },
+    render: function() {
+        return (
+            <form>
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    value={this.props.filterText}
+                    ref="filterTextInput"
+                    onChange={this.handleChange}
+                />
+            </form>
+        );
+    }
 });
--- server/app.js
+++ server/app.js
@@ -1,13 +1,35 @@
-Posts.remove({});
-Posts.insert({
-  _id: "hello-world",
-  title: "Hello World Post",
-  content: "This is the content"
+Meteor.startup(function() {
+    // Posts.remove({});
+    for (i = Posts.find().count(); i < 10000; i++) {
+        Posts.insert({
+            _id: 'p' + i,
+            user: 'user' + i + ' ' + faker.internet.userName(),
+            title: 'title' + i + ' ' + faker.lorem.sentence(),
+            content: 'content' + i + ' ' + faker.lorem.paragraphs() + ' ' + faker.lorem.paragraphs() + ' ' + faker.lorem.paragraphs(),
+            createdAt: Date.now()
+        });
+    }
 });
 
+
 Meteor.publish('singlePost', function(id) {
-  check(id, String);
-  // Make a delay manually to show the loading state
-  Meteor._sleepForMs(1000);
-  return Posts.find({_id: id});
+    check(id, String);
+    // Make a delay manually to show the loading state
+    Meteor._sleepForMs(1000);
+    return Posts.find({
+        _id: id
+    });
+});
+
+Meteor.publish('allPosts', function(id) {
+    // Make a delay manually to show the loading state
+    console.log('waiting... ')
+    Meteor._sleepForMs(2500);
+    console.log('subscribed to all posts')
+    return Posts.find({}, {
+        fields: {
+            title: 1,
+            createdAt: 1
+        }
+    });
 });
